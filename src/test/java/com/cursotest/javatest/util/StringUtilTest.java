6981453fa99f4f15b30e5eb2ab3ad6e0fea8a148
Alex Acevedo <alex.acpe@outlook.com>
2022-06-28 09:58:11 -0500

    Mensaje de test

--- src/test/java/com/cursotest/javatest/util/StringUtilTest.java
+++ src/test/java/com/cursotest/javatest/util/StringUtilTest.java
@@ -3,8 +3,18 @@ package com.cursotest.javatest.util;
 //prueba unitaria manual para repetir strings
 public class StringUtilTest {
     public static void main(String[] args) {
+
         //se llama la funcion a la que se le va a realizar el test
         String result = StringUtil.repeat("Saludo, ",5);
-        System.out.println(result);
+        if(result.equals("Saludo, Saludo, Saludo, Saludo, Saludo,")){
+            System.out.println("OK");
+        } else {
+            System.out.println("no son los parametros");
+        }
+
+        String result2 = StringUtil.repeat("Saludo, ",1);
+        if(result2.equals("Saludo, ")){
+            System.out.println("OK");
+        }
     }
 }
