4cde889c5aa94cd225c50e207778fdb7a2d2c8b4
Alex Acevedo <alex.acpe@outlook.com>
2022-06-28 14:26:52 -0500

    Creación de test unitario: lanzar una excepción para alertar sobre un error

--- src/test/java/com/cursotest/javatest/util/StringUtilTest.java
+++ src/test/java/com/cursotest/javatest/util/StringUtilTest.java
@@ -5,16 +5,24 @@ public class StringUtilTest {
     public static void main(String[] args) {
 
         //se llama la funcion a la que se le va a realizar el test
-        String result = StringUtil.repeat("Saludo, ",5);
-        if(result.equals("Saludo, Saludo, Saludo, Saludo, Saludo,")){
-            System.out.println("OK");
-        } else {
-            System.out.println("no son los parametros");
+        String result = StringUtil.repeat("Saludo, ", 5);
+        if (!result.equals("Saludo, Saludo, Saludo, Saludo, Saludo, ")) {
+            //lanzar excepcion para mostrar el error
+            throw new RuntimeException("ERROR");
+        }else{
+            System.out.println("---compilacion completa");
         }
 
-        String result2 = StringUtil.repeat("Saludo, ",1);
-        if(result2.equals("Saludo, ")){
-            System.out.println("OK");
+        //crear funcion con mas informacion, lo que resulta con lo esperado
+        String result2 = StringUtil.repeat("Saludo2, ", 3);
+
+        assertEquals(result2,"Saludo2, Saludo2, Saludo2, sa");
+    }
+        //funcion con parametros valor actual y valor esperado
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)) {
+            //lanzar excepcion para mostrar el error
+            throw new RuntimeException(actual + "no es igual al valor esperado: "+expected);
         }
     }
 }
